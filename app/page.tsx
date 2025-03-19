9f9cf6a9383da2f6a375d8dc278f031544f5b2c3
pitonworks <your.email@example.com>
2025-03-19 20:33:56 +0200

    lang fix

--- app/page.tsx
+++ app/page.tsx
@@ -185,7 +185,6 @@ export default function HomePage() {
                   <path d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z"/>
                 </svg>
               </a>
-              
               <a href="https://www.tripadvisor.com/Restaurant_Review-g4563675-d4752816-Reviews-Eagle_s_Nest_Restaurant_Bar-Tatlisu_Kyrenia_District.html" className="hover:text-gray-900">
                 <svg className="w-6 h-5 link-ta" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -197,9 +196,9 @@ export default function HomePage() {
                   <path d="M9 6.75c2 -.667 4 -.667 6 0" />
                 </svg>
               </a>
-              <div className="flex items-center">
-                <LanguageToggle />
-              </div>
+            </div>
+            <div className="flex justify-center items-center mt-2">
+              <LanguageToggle />
             </div>
             <div className="w-full max-w-md searchBox cFullW">
               <div className="relative">
